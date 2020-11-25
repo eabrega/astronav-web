c978edca082674acedf48364574a0f37d4b3ff87
eabrega <eabrega@gmail.com>
2020-11-26 00:43:46 +0300

    init

--- src/store/index.ts
+++ src/store/index.ts
@@ -4,12 +4,24 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+    state: {
+        condition: {}
+    },
+    mutations: {
+        'SET_STORE'(state, data) {
+            state.condition = data
+        }
+    },
+    actions: {
+        initStore: ({ commit }) => {
+            fetch(`https://astronav.ru/condition/date/22-06-1985/latitude/55/longitude/33`)
+            .then(response => response.json())
+            .then(data => commit('SET_STORE', data));
+        }
+    },
+    modules: {
+    },
+    getters: {
+        data: state => state.condition
+    }
 })
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
 </template>
@@ -8,11 +7,17 @@
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import store from '@/store';
 
 @Component({
   components: {
     HelloWorld,
   },
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  constructor(){
+    super()
+    store.dispatch('initStore')
+  }
+}
 </script>
