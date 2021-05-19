0de2c2299e545bcb18069eee658cecb9a7370d30
Evgeny Brega <eabrega@gmail.com>
2021-05-19 10:48:04 +0300

    fixed

--- vue.config.js
+++ vue.config.js
@@ -12,7 +12,7 @@ const robotsOptions = {
         },
         {
             userAgent: "PetalBot",
-            disallow: "/"
+            allow: "/about",
         }
     ],
     sitemap: `${hostName}/sitemap.xml`,
