acf8dc544c64759525023dbf3b55b3aee039929b
Brega Evgeny <eabrega@gmail.com>
2021-09-05 23:00:38 +0300

    infoBar component created

--- src/App.vue
+++ src/App.vue
@@ -23,41 +23,23 @@
                 <AppSettingsSidebar />
 
                 <b-alert
-                    class="info_bar"
                     variant="success"
                     v-model="isShowMessageBox"
                     dismissible
                 >
                     На странице представленна информация об астрономических
                     объектах видимых с учетом вашего
-                    <b-link v-b-toggle.app-settings-sidebar
-                        >местоположения</b-link
-                    >.<br />
+                    <b-link v-b-toggle.app-settings-sidebar>
+                        местоположения
+                    </b-link>
+                    .
+                    <br />
                     Выбранное местоположение сохранится в настройках браузера.
                     <br />
                     Вы всегда можете изменить отображение подсказок в
                     <b-link v-b-toggle.app-settings-sidebar>настройках</b-link>.
                 </b-alert>
-                <div class="schedule-info_bar">
-                    <b-alert class="info_bar" variant="secondary" show>
-                        <div class="schedule-latlon">
-                            <span>Широта: {{ $store.state.lat }}</span>
-                            <span>Долгота: {{ $store.state.lon }}</span>
-                        </div>
-                    </b-alert>
-                    <b-alert class="info_bar" variant="secondary" show>
-                        <div class="schedule-date">
-                            <b>{{ REQUEST_DATE }}</b>
-                            <span class="schedule-time" v-if="IS_TODAY">
-                                {{ CURRENT_TIME }}
-                            </span>
-                            <span class="schedule-date__suffix">
-                                {{ DATE_SUFFIX }}
-                            </span>
-                        </div>
-                    </b-alert>
-                </div>
-
+                <InfoBar />
                 <router-view />
             </div>
         </div>
@@ -67,6 +49,7 @@
 <script lang="ts">
 import AppSettingsSidebar from "@/components/Common/AppSettingsSidebar.vue";
 import { Component, Vue } from "vue-property-decorator";
+import InfoBar from "@/components/Common/InfoBar.vue";
 @Component({
     name: "App",
     metaInfo: {
@@ -90,56 +73,10 @@ import { Component, Vue } from "vue-property-decorator";
     },
     components: {
         AppSettingsSidebar,
+        InfoBar,
     },
 })
 export default class App extends Vue {
-    private time = new Date();
-
-    mounted() {
-        this.runTimeUpdate();
-    }
-
-    get CURRENT_TIME() {
-        return this.time.toLocaleTimeString();
-    }
-
-    get REQUEST_DATE() {
-        return (this.$store.state.date as Date).toLocaleDateString();
-    }
-
-    get IS_TODAY() {
-        const requestDate = this.$store.state.date as Date;
-        return this.dDays(requestDate) == 0 ? true : false;
-    }
-
-    get DATE_SUFFIX() {
-        let resString = "";
-        const requestDate = this.$store.state.date as Date;
-        const dD = this.dDays(requestDate);
-
-        if (this.IS_TODAY) {
-            return "";
-        }
-
-        const suffix = this.declOfNum(dD, ["день", "дня", "дней"]);
-
-        switch (dD) {
-            case -1:
-                resString = "вчера";
-                break;
-            case 1:
-                resString = "завтра";
-                break;
-            default:
-                resString =
-                    dD < 0
-                        ? `${dD * -1} ${suffix} назад`
-                        : `через ${dD} ${suffix}`;
-        }
-
-        return `(${resString})`;
-    }
-
     get isShowMessageBox() {
         if ((window as any)?.prerenderInjected == "false") {
             return false;
@@ -149,30 +86,6 @@ export default class App extends Vue {
 
     set isShowMessageBox(val: boolean) {
         this.$store.dispatch("setIsShowHelpMessage", false);
-    }
-
-    private runTimeUpdate() {
-        setInterval(() => {
-            this.time = new Date();
-        }, 1000);
-    }
-
-    private declOfNum(number: number, titles: Array<string>) {
-        number = Math.abs(number);
-        const cases = [2, 0, 1, 1, 1, 2];
-        return titles[
-            number % 100 > 4 && number % 100 < 20
-                ? 2
-                : cases[number % 10 < 5 ? number % 10 : 5]
-        ];
-    }
-
-    private dDays(distinationDate: Date): number {
-        const currentDate = new Date();
-        const dDays = Math.round(
-            (distinationDate.getTime() - currentDate.getTime()) / 3600000 / 24
-        );
-        return dDays;
     }
 }
 </script>
@@ -193,8 +106,6 @@ export default class App extends Vue {
         --main-margin: 10px;
     }
 }
-
-$font-size: 1.4em;
 
 #app {
     min-width: var(--min-size);
@@ -221,54 +132,12 @@ $font-size: 1.4em;
     .main {
         display: flex;
         justify-content: center;
-        //flex-basis: 1200px;
 
         .wrap {
             max-width: calc(var(--max-size));
             min-width: calc(var(--min-size) - var(--main-margin));
             margin-left: var(--main-margin);
             margin-right: var(--main-margin);
-
-            .schedule-info_bar {
-                display: flex;
-                justify-content: space-between;
-                flex-wrap: wrap;
-                margin-right: calc(var(--main-margin) * -1);
-
-                .info_bar {
-                    min-width: calc(var(--min-size) - var(--main-margin));
-                    flex-basis: 390px;
-                    flex-grow: 1;
-                    margin-right: var(--main-margin);
-
-                    .schedule-time {
-                        margin-left: 15px;
-                    }
-                    .schedule-date {
-                        font-size: $font-size;
-
-                        .schedule-date__suffix {
-                            margin-left: 15px;
-                            font-size: 0.8em;
-                        }
-                    }
-                }
-
-                .info_bar:first-of-type {
-                    min-width: calc(var(--min-size) - var(--main-margin));
-                    flex-basis: 450px;
-                    margin-right: var(--main-margin);
-
-                    .schedule-latlon {
-                        display: flex;
-                        flex-wrap: wrap;
-                        font-size: $font-size;
-                        span {
-                            padding-right: 20px;
-                        }
-                    }
-                }
-            }
         }
     }
 
---
+++ src/components/Common/InfoBar.vue
@@ -0,0 +1,144 @@
+<template>
+    <div class="info_bar">
+        <b-alert class="info_bar-panel" variant="secondary" show>
+            <div class="latlon">
+                <span>Широта: {{ $store.state.lat }}</span>
+                <span>Долгота: {{ $store.state.lon }}</span>
+            </div>
+        </b-alert>
+        <b-alert class="info_bar-panel" variant="secondary" show>
+            <div class="date">
+                <b>{{ REQUEST_DATE }}</b>
+                <span class="date-time" v-if="IS_TODAY">
+                    {{ CURRENT_TIME }}
+                </span>
+                <span class="date__suffix">
+                    {{ DATE_SUFFIX }}
+                </span>
+            </div>
+        </b-alert>
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class InfoBar extends Vue {
+    private time = new Date();
+
+    mounted() {
+        this.runTimeUpdate();
+    }
+
+    get CURRENT_TIME() {
+        return this.time.toLocaleTimeString();
+    }
+
+    get REQUEST_DATE() {
+        return (this.$store.state.date as Date).toLocaleDateString();
+    }
+
+    get IS_TODAY() {
+        const requestDate = this.$store.state.date as Date;
+        return this.dDays(requestDate) == 0 ? true : false;
+    }
+
+    get DATE_SUFFIX() {
+        let resString = "";
+        const requestDate = this.$store.state.date as Date;
+        const dD = this.dDays(requestDate);
+
+        if (this.IS_TODAY) {
+            return "";
+        }
+
+        const suffix = this.declOfNum(dD, ["день", "дня", "дней"]);
+
+        switch (dD) {
+            case -1:
+                resString = "вчера";
+                break;
+            case 1:
+                resString = "завтра";
+                break;
+            default:
+                resString =
+                    dD < 0
+                        ? `${dD * -1} ${suffix} назад`
+                        : `через ${dD} ${suffix}`;
+        }
+
+        return `(${resString})`;
+    }
+
+    private declOfNum(number: number, titles: Array<string>) {
+        number = Math.abs(number);
+        const cases = [2, 0, 1, 1, 1, 2];
+        return titles[
+            number % 100 > 4 && number % 100 < 20
+                ? 2
+                : cases[number % 10 < 5 ? number % 10 : 5]
+        ];
+    }
+
+    private dDays(distinationDate: Date): number {
+        const currentDate = new Date();
+        const dDays = Math.round(
+            (distinationDate.getTime() - currentDate.getTime()) / 3600000 / 24
+        );
+        return dDays;
+    }
+
+    private runTimeUpdate() {
+        setInterval(() => {
+            this.time = new Date();
+        }, 1000);
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+$font-size: 1.4em;
+
+.info_bar {
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+    margin-right: calc(var(--main-margin) * -1);
+
+    .info_bar-panel {
+        min-width: calc(var(--min-size) - var(--main-margin));
+        flex-basis: 390px;
+        flex-grow: 1;
+        margin-right: var(--main-margin);
+
+        &:first-of-type {
+            flex-basis: 450px;
+        }
+
+        .date {
+            font-size: $font-size;
+
+            .date-time {
+                margin-left: 15px;
+            }
+
+            .date__suffix {
+                margin-left: 15px;
+                font-size: 0.8em;
+            }
+        }
+
+        .latlon {
+            display: flex;
+            flex-wrap: wrap;
+            font-size: $font-size;
+
+            span {
+                padding-right: 20px;
+            }
+        }
+    }
+}
+</style>
