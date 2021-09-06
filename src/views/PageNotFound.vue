e269df20b0aac4ca83a0527b88e70a38aef18afc
Brega Evgeny <eabrega@gmail.com>
2021-09-07 01:21:01 +0300

    orfo

--- src/views/PageNotFound.vue
+++ src/views/PageNotFound.vue
@@ -15,14 +15,12 @@ export default {
 
 <style lang="scss">
 .page-not-found {
-    //width: 1200px !important;
     h1 {
         font-size: 8em !important;
         text-align: center;
     }
     p {
         font-size: 1.2em !important;
-       // width: 1200px !important;
     }
 }
 </style>
